256b75b34fab2a60460ffae03f56161277c35f51
Aviraj Khare <thisisavirajkhare@gmail.com>
2023-01-10 23:48:02 +0530

    [Aviraj] Added jinja2 code generation and tests

--- invoker.py
+++ invoker.py
@@ -2,9 +2,13 @@ from template_generators.flipper import Flipper
 
 
 class Invoker:
-    def __init__(self):
+    def __init__(self) -> None:
         pass
 
     @staticmethod
-    def flipper():
+    def flipper() -> None:
         Flipper.generate_code()
+
+    @staticmethod
+    def psp22(**kwargs) -> None:
+        pass
---
+++ tests/template_generators/test_flipper.py
@@ -0,0 +1,31 @@
+import unittest
+import hashlib
+import shutil
+import os
+
+from template_generators.flipper import Flipper
+
+class TestFlipper(unittest.TestCase):
+
+    def setUp(self) -> None:
+        if os.path.isdir("flipper"):
+            shutil.rmtree("flipper")
+        Flipper.generate_code()
+
+    def tearDown(self) -> None:
+        shutil.rmtree("flipper")
+
+    def test_dir_exists(self) -> None:
+        self.assertEqual(os.path.isdir("flipper"), True)
+
+    def test_file_exists(self) -> None:
+        self.assertEqual(os.path.isfile("flipper/lib.rs"), True)
+        self.assertEqual(os.path.isfile("flipper/Cargo.toml"), True)
+
+    def test_file_md5(self) -> None:
+        contract = open("flipper/lib.rs", "rb")
+        toml = open("flipper/Cargo.toml", "rb")
+        self.assertEqual(hashlib.md5(contract.read()).hexdigest(), "841fad71d8f7d5f14bc063bd75b47263")
+        self.assertEqual(hashlib.md5(toml.read()).hexdigest(), "48953a9a94e285ce1b33551960fcdba8")
+        contract.close()
+        toml.close()
