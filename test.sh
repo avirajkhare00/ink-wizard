5206d29205f1bfad0acdafd57e4574d3ede8ec4c
Aviraj Khare <thisisavirajkhare@gmail.com>
2023-01-14 00:06:43 +0530

    [Aviraj] Added test.sh file

---
+++ test.sh
@@ -0,0 +1,7 @@
+./.venv/bin/python -m unittest tests/template_generators/test_flipper.py
+./.venv/bin/python -m unittest tests/template_generators/test_psp22.py
+./.venv/bin/python -m unittest tests/template_generators/test_psp34.py
+./.venv/bin/python -m unittest tests/commands/test_template_select.py
+./.venv/bin/python -m unittest tests/commands/test_flipper.py
+./.venv/bin/python -m unittest tests/commands/test_psp22.py
+./.venv/bin/python -m unittest tests/commands/test_psp34.py
